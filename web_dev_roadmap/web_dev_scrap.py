75986e822d4f46b4acace0e134157aeac3b1e682
CodeVerse-Ro <rxhanprakash22@gmail.com>
2024-01-15 17:30:15 +0530

    Scrapped_1_Site

--- web_dev_roadmap/web_dev_scrap.py
+++ web_dev_roadmap/web_dev_scrap.py
@@ -1,4 +1,36 @@
-
 import requests
-url = ""
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.browserstack.com/guide/web-development-roadmap"
+
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content,'html.parser')
+
+
+# THIS CONTAINS THE MAIN HEADING FOR EACH STEPS OF ROADMAP
+h4_elements = soup.find_all('h4')
+h4_contents = [h4.text for h4 in h4_elements]
+# print(h4_contents)    #success
+
+
+# THIS CONTAINS BASIC INFORMATION REGARDING EACH STEPS 
+p_elements = soup.find_all('span')
+p_contents = [p.text for p in p_elements]
+# print(p_contents)
+
+
+# THIS CONTAINS HOW TO APPROACH EACH STEPS IN ROADMAP
+li_elements = soup.find_all('li')
+span_contents = [li.find('span').text if li.find('span') else '' for li in li_elements]
+# print(span_contents)
+
+
+
+# THIS CONTAINS {THIS PART} : ----
+b_elements = soup.find_all('b')
+b_contents = [b.text for b in b_elements]
+# print(b_contents)
 
